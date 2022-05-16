dd0535f5cdad71fa1f6a136d1caadd6915c1f506
Agnes <koinageagness@gmail.com>
2022-05-16 07:41:12 +0300

    install flask login and initialize it in init .py

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import config_options
+from flask_login import LoginManager
 
 bootstrap = Bootstrap()
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
 def create_app(config_name):
 
@@ -13,6 +17,8 @@ def create_app(config_name):
 
     # Initializing flask extensions
     bootstrap.init_app(app)
+    db.init_app(app)
+    login_manager.init_app(app)
 
     # Registering the blueprint
     from .main import main as main_blueprint
