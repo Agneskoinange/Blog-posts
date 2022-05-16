07c3a7066fd011846830ed7564ba255e8bf2bbc1
Agnes <koinageagness@gmail.com>
2022-05-16 21:31:47 +0300

    modify gitgonre and init.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,13 @@
+
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import config_options
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_sqlalchemy import SQLAlchemy
+from config import Config
+from config import DevConfig
+from flask_uploads import UploadSet,configure_uploads,IMAGES
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
@@ -11,6 +15,8 @@ login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
 mail = Mail()
+app.config.from_object(Config)
+photos = UploadSet('photos',IMAGES)
 
 def create_app(config_name):
 
--- app/main/forms.py
+++ app/main/forms.py
@@ -2,7 +2,6 @@ from flask_wtf import FlaskForm
 from wtforms import StringField,SubmitField,TextAreaField
 from wtforms.validators import DataRequired
 
-
 class CreateBLog(FlaskForm):
     name = StringField('Your Name',validators=[DataRequired()])
     title = StringField('Title',validators =[DataRequired()])
